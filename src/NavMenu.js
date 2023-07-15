49ca9ced1ad065558f39b4e46c7e819ebf0f7aee
Jonas Kaatz <jonas@jonaskaatz.com>
2023-07-15 08:32:39 +0200

    change components

---
+++ src/NavMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavMenu({lang,handleLangChange,handleNavChange}) {
+    return (
+        <div className="flex flex-col md:flex-row items-center w-full md:w-auto py-2">
+            <button onClick={()=>handleNavChange('Home')} className="hover:shadow-lg shadow-cyan-200 rounded px-4 py-2 text-white w-5/6 md:w-auto">Home</button>
+            <button onClick={()=>handleNavChange('About')} className="hover:shadow-lg shadow-cyan-200 rounded px-4 py-2 text-white w-5/6 md:w-auto">About</button>
+            <button onClick={()=>handleNavChange('Projects')} className="hover:shadow-lg shadow-cyan-200 rounded px-4 py-2 text-white w-5/6 md:w-auto">Projects</button>
+            {(lang==='de')?<button onClick={()=>handleLangChange('en')} className='md:relative md:top-1 text-3xl'><span className='flag'>{'🇬🇧'}</span></button>:<button onClick={()=>handleLangChange('de')} className='md:relative md:top-1 text-3xl'><span className='flag'>{'🇩🇪'}</span></button>}
+        </div>
+    )
+}
---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <div className="flex flex-col lg:flex-row lg:mt-9 w-11/12 md:w-3/4 lg:w-3/4 bg-white rounded-3xl p-6 shadow-xl shadow-slate-200">
+            Projects
+        </div>
+    )
+}
