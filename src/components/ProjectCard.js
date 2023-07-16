675261621318a2117e1eeb692bad3c55003f638e
Jonas Kaatz <jonas@jonaskaatz.com>
2023-07-16 11:05:03 +0200

    changed project overview

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,7 +8,6 @@ export default function ProjectCard({lang}) {
             <h3 className="text-xl mb-2">Name</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <a href="url" target='_blank' rel="noreferrer" className="flex justify-center items-center mt-2 py-2 w-20 hover:scale-125 ease-in-out duration-200 delay-75 text-sm text-center bg-indigo-500 shadow-lg shadow-indigo-300 rounded-lg text-white"><FiShare className="text-xl mr-1"/> {L['open'][lang]}</a>
-
         </div>
     )
 }
