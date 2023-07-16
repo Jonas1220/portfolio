c0e50baf9fa3d9394fabc4f336c52bf90cb162a7
Jonas Kaatz <jonas@jonaskaatz.com>
2023-07-16 11:03:58 +0200

    clean code

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,6 @@ import { FaTwitter,FaGithub,FaLinkedinIn } from "react-icons/fa6";
 import { PiEnvelopeSimple,PiPhone } from "react-icons/pi";
 import L from "./L.json"
 
-
-// const L = {
-//     welcome:{de:'Willkommen, ich bin ',en:'Welcome, I\'m'},
-//     text:{de:['Ich bin Software Entwickler aus der Nähe von Frankfurt mit Erfahrungen im Entwickeln von Full Stack Web Applikationen, iOS und Android Apps.','Stand 2023 arbeite ich seit mehr als 10 Jahren im Tech-Industrie. Designen und programmieren sind, seit ich mit dem Arbeiten am Computer angefangen habe, meine große Leidenschaft. Ich liebe es, schön gestaltete, intuitive und funktionale Websites und Apps zu erstellen.','Außerdem interessiere ich mich leidenschaftlich für Krypto, Reisen und das Lernen neuer Dinge.'],en:['I’m a software engineer based near Frankfurt, Germany with experience in building full stack web applications, iOS & android development.','As of 2023 I worked in the tech industry for over ten years. Designing and Coding have been my passion since the day I started working with computers. I love creating beautifully designed, intuitive and functional websites and apps.','Beside that I’m passionate about crypto, travel and exploring new things.']},
-//     email:{de:'hi@jonaskaatz.com',en:'hi@jonaskaatz.com'},
-//     skills:{de:{Kompetent:'php, javascript, bootstrap, html, css, react, react native, mysql',Erfahrung:'python, expo, git, mongodb',Sonstiges:'Krypto, Kaffee, Reisen'},en:{proficient:'php, javascript, bootstrap, html, css, react, react native, mysql',experience:'python, expo, git, mongodb',other:'crypto, coffee, travel'}},
-//     about:{de:'Über mich',en:'About me'},
-//     projects:{de:'Projekte',en:'Projects'},
-// }
-
 export default function Home({lang}) {
     return (
         <div className="flex flex-col lg:flex-row lg:mt-28 w-11/12 md:w-3/4 lg:w-2/3 bg-white rounded-3xl p-6 shadow-xl shadow-slate-200 max-w-7xl">
