353f03f55272897692c27f3551da0a87542fa54f
Jonas Kaatz <jonas@jonaskaatz.com>
2023-07-17 09:06:02 +0200

    added project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,12 +2,12 @@ import React from 'react'
 import L from "./L.json"
 import { FiShare } from "react-icons/fi";
 
-export default function ProjectCard({lang}) {
+export default function ProjectCard({name,description,url,lang}) {
     return (
-        <div className="bg-blue-100 w-full p-5 mb-4">
-            <h3 className="text-xl mb-2">Name</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <a href="url" target='_blank' rel="noreferrer" className="flex justify-center items-center mt-2 py-2 w-20 hover:scale-125 ease-in-out duration-200 delay-75 text-sm text-center bg-indigo-500 shadow-lg shadow-indigo-300 rounded-lg text-white"><FiShare className="text-xl mr-1"/> {L['open'][lang]}</a>
+        <div className="bg-blsue-100 w-full p-5 mbss-4">
+            <h3 className="text-2xl mb-2">{name}</h3>
+            <p>{description[lang]}</p>
+            <a href={url} target='_blank' rel="noreferrer" className="flex justify-center items-center mt-2 py-2 w-24 hover:scale-125 ease-in-out duration-200 delay-75 text-sm text-center bg-indigo-500 shadow-lg shadow-indigo-300 rounded-lg text-white"><FiShare className="text-xl mr-1"/> {L['open'][lang]}</a>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
+import L from "./L.json"
 
 export default function Projects({lang}) {
     return (
         <div className="flex flex-col lg:mt-28 w-11/12 md:w-3/4 lg:w-2/3 bg-white rounded-3xl p-6 shadow-xl shadow-slate-200 max-w-7xl">
-            <ProjectCard lang={lang} />
-            <ProjectCard lang={lang} />
-            <ProjectCard lang={lang} />
-            <ProjectCard lang={lang} />
+            {Object.keys(L.projects.projectCards).map(project=>{
+                return <ProjectCard name={project} description={L['projects']['projectCards'][project]} url={L['projects']['projectCards'][project]['url']} lang={lang} />
+            })}
         </div>
     )
 }
