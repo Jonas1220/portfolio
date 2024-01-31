7e4b8d8039027b05b4c452b2e863915fa997c642
Jonas Kaatz <jonas@jonaskaatz.com>
2024-01-31 17:42:17 +0100

    x

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,7 +9,7 @@ export default function ProjectCard({name,description,url,lang,img}) {
                 <img className='max-w-[100px] mb-2' src={require("../img/"+img+".png")} alt={img+' logo'}/>
                 <h3 className="text-center text-2xl mb-2">{name}</h3>
                 <p className="text-center">{description[lang]}</p>
-                <a href={url} target='_blank' rel="noreferrer" className="flex justify-center items-center mt-2 py-2 w-24 hover:scale-125 ease-in-out duration-200 delay-75 text-sm text-center bg-indigo-500 shadow-lg shadow-indigo-300 rounded-lg text-white"><FiShare className="text-xl mr-1"/> {L['open'][lang]}</a>
+                <a href={url} target='_blank' rel="noreferrer" className="flex justify-center items-center mt-2 py-2 w-24 hover:scale-125 ease-in-out duration-200 delay-75 text-sm text-center bg-indigo-600 shadow-lg shadow-indigo-900 rounded-lg text-white"><FiShare className="text-xl mr-1"/> {L['open'][lang]}</a>
             </div>
         </div>
     )
